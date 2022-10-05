85715a04b3ae9dcb269fc9390bdd1b2bc11c55e0
Nihar Hegde <59167599+nihar-hegde@users.noreply.github.com>
2022-10-05 17:20:06 +0530

    adding components and containers

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import {About,Footer,Header,Work,Skills,Testimonial} from './container';
+import { Navbar } from './components';
+
 const App = () => {
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/>
+    </div>
   );
 }
 export default App;
